9830e6777501d7e82fec89c13e40f98162de3755
Idris Zahir <izahir@lakeheadu.ca>
2026-02-22 00:05:25 -0500

    Add project files.

---
+++ CraftCellar/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CraftCellar.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
